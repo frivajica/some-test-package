4c43b1c89e806fbf7a43c545ac1559961de16ee2
Fran <franivan.j.c@gmail.com>
2024-10-07 17:57:07 -0500

    initial commit.

---
+++ src/code.ts
@@ -0,0 +1,4 @@
+export const test = 'This is within the package, but is not printed within the package.'
+export const sumTwoNumbers = (a: number, b: number) => a + b
+
+console.log('This is printed within the package.')
